ddee9026827f2b90b6a2756295f5c6216e6bf67d
Sara <sarakang05@gmail.com>
2019-01-12 20:34:03 -0800

    first commit

---
+++ food.js
@@ -0,0 +1,7 @@
+exports.food =  function() {
+    console.log("Enjoy this delicious food");
+}
+
+exports.cafe = function(){
+    console.log("Come check out the cutest cafe");
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+const greetings = require("./greeting.js");
+const foods = require("./food.js");
+// "Hello"
+greetings.greetingEnglish();
+// "Konichiwa"
+greetings.greetingJapanese();
+//"Hola"
+greetings.greetingSpanish();
+//"Hallo"
+greetings.greetingIcelandic();
+
+foods.food();
+
+foods.cafe();
